44c8d340f20cba0f1985521bfc06b45bccfea866
Leandro Franchi <leandro.franchi@gmail.com>
2022-01-26 12:02:41 -0300

    Plus Minus Ratio Problem.

---
+++ src/main/java/br/com/lefranchi/algo/plusminus/PlusMinusSolution.java
@@ -0,0 +1,27 @@
+package br.com.lefranchi.algo.plusminus;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class PlusMinusSolution {
+
+    public static void main(String[] args) {
+
+        List<Integer> arr = Arrays.asList(-4, 3, -9, 0, 4, 1);
+
+        long n = arr.size();
+        long positiveCount = arr.stream().filter(x -> x > 0).count();
+        long negetiveCount = arr.stream().filter(x -> x < 0).count();
+        long zeroCount = arr.stream().filter(x -> x == 0).count();
+
+        float positiveRatio = (float) positiveCount / n;
+        float negativeRatio = (float) negetiveCount / n;
+        float zeroRatio = (float) zeroCount / n;
+
+        System.out.printf("%.6f \n", positiveRatio);
+        System.out.printf("%.6f \n", negativeRatio);
+        System.out.printf("%.6f", zeroRatio);
+
+    }
+
+}
