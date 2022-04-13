23c9de7b26b1c79db19fb9d6c65bdfaf166ce183
Leandro Franchi <leandro.franchi@gmail.com>
2022-04-13 09:45:16 -0300

    Breadth First.

---
+++ src/main/python/breadth_first_search.py
@@ -0,0 +1,39 @@
+"""
+Search on Graph implementation using Breath-first Search Algorithm!
+"""
+
+from collections import deque
+
+
+def person_is_seller(name):
+    return name[-1] == 'm'
+
+
+def search(name):
+
+    search_queue = deque()
+    search_queue += graph[name]
+    searched = []
+
+    while search_queue:
+        person = search_queue.popleft()
+        if person_is_seller(person):
+            print(person + " is a seller!")
+            return True
+        else:
+            search_queue += graph[person]
+            searched.append(person)
+    return False
+
+
+graph = {}
+graph["you"] = ["alice", "bob", "claire"]
+graph["bob"] = ["anuj", "peggy"]
+graph["alice"] = ["peggy"]
+graph["claire"] = ["thom", "jonny"]
+graph["anuj"] = []
+graph["peggy"] = []
+graph["tom"] = []
+graph["jonny"] = []
+
+search("you")
